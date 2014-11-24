a6eb4ec457eb4b2a5aa5c062740b0a828356052e
ThomasHenderson <admin@adblockcash.org>
2014-11-24 09:31:37 +0100

    Install gulp to implement watch & compile task

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+var shell = require("gulp-shell");
+
+gulp.task("build-devenv", shell.task("./build.py -t chrome devenv"));
+
+gulp.task("watch", function(){
+  gulp.watch([
+    "chrome/**",
+    "ext/**",
+    "lib/**",
+    "skin/**",
+    "*.{html,js}",
+  ], ["build-devenv"]);
+})
+
+gulp.task("default", ["build-devenv", "watch"]);
