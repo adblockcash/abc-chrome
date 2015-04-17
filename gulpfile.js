deab801db1848bf7a519d4062340a6bc8e21234a
ThomasHenderson <admin@adblockcash.org>
2015-04-17 15:36:01 +0200

    Release task for firefox

--- gulpfile.js
+++ gulpfile.js
@@ -131,12 +131,14 @@ gulp.task("build-dist", function(callback) {
 
 if (GLOBALS.PLATFORM == "gecko") {
   // gulp.task("buildtools:autoinstall", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" autoinstall 8888"));
-  gulp.task("buildtools:autoinstall", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" build && rm -rf xpi && mkdir xpi && mv *.xpi xpi/ && cd xpi/ && unzip *.xpi && cfx xpi --output-file cfx-build.xpi && (wget -q --post-file=cfx-build.xpi http://localhost:8888/ || exit 0)"));
+  gulp.task("buildtools:autoinstall", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" build && rm -rf xpi && mkdir xpi && mv *.xpi xpi/ && cd xpi/ && unzip *.xpi && cfx xpi --output-file abc-firefox.xpi && (wget -q --post-file=abc-firefox.xpi http://localhost:8888/ || exit 0)"));
   gulp.task("buildtools:devenv", ["buildtools:autoinstall"]);
 
   gulp.task("buildtools:run", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" build && rm -rf xpi && mkdir xpi && mv *.xpi xpi/ && cd xpi/ && unzip *.xpi && cfx run"));
 
-  gulp.task("buildtools:build-release", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" build -r -b 0 && rm -rf xpi && mkdir xpi && mv *.xpi xpi/ && cd xpi/ && unzip *.xpi && cfx xpi && (wget -q --post-file=abc-firefox.xpi http://localhost:8888/ || exit 0)"));
+  gulp.task("buildtools:build", ["buildtools:autoinstall"]);
+
+  gulp.task("buildtools:build-release", shell.task("cd adblockcash && ./build.py -t "+ GLOBALS.PLATFORM +" build -r -b 0 && rm -rf xpi && mkdir xpi && mv *.xpi xpi/ && cd xpi/ && unzip *.xpi && cfx xpi --output-file abc-firefox.xpi && (wget -q --post-file=abc-firefox.xpi http://localhost:8888/ || exit 0)"));
 } else {
   gulp.task("buildtools:devenv", shell.task("rm -rf devenv/* && ./build.py -t "+ GLOBALS.PLATFORM +" devenv"));
 
